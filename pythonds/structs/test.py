975f2ce9573122e8f49bb8dab0cc40b53509342b
Marco Orban <marco.orban@gmail.com>
2021-11-08 16:20:14 +0800

    deleted unnecessary file and create unittest for heap

--- pythonds/structs/test.py
+++ pythonds/structs/test.py
@@ -1,10 +1,31 @@
 import unittest
 import binheap
 
-a = binheap.BinHeap()
-a.insert(3)
-a.insert(5)
-a.insert(7)
-a.insert(2)
-print(a.currentSize)
-print(a.heapList)
+class TestHeap(unittest.TestCase):
+
+    def setUp(self):
+        self.heap = binheap.BinHeap()
+
+    def test_insert_one(self):
+        self.heap.insert(3)
+        self.assertEqual(self.heap.heapList, [0, 3])
+
+    def test_insert_many(self):
+        self.heap.insert(3)
+        self.heap.insert(1)
+        self.heap.insert(2)
+        self.heap.insert(5)
+        self.assertEqual(self.heap.heapList, [0,1,3,2,5])
+        self.assertEqual(self.heap.currentSize, 4)
+
+    def test_delMin(self):
+        self.heap.insert(3)
+        self.heap.insert(1)
+        self.heap.insert(2)
+        self.heap.insert(5)
+        min = self.heap.delMin()
+        self.assertEqual(min, 1)
+        self.assertEqual(self.heap.heapList, [0,2,3,5])    
+
+if __name__ == "__main__":
+    unittest.main()
